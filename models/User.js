02b2e6647ff309831e77d63dc1248cab448b1034
Chathura Ranasinghe <chathurar.99.bc@gmail.com>
2023-10-29 11:58:56 +0530

    controllers

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     roles: [{
         type: String,
-        default: "student"
+        default: "Employee"
     }],
     active: {
         type: Boolean,
