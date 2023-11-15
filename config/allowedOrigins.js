9db2f5433900cb1275385d4c1c6e0409f3e1a1e6
Chathura Ranasinghe <chathurar.99.bc@gmail.com>
2023-11-15 14:20:37 +0530

    url update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,14 +1,5 @@
-// Define allowed origins
-
-// Define an array of allowed origins for Cross-Origin Resource Sharing (CORS)
 const allowedOrigins = [
-    
-    // Add the origin where the React app is hosted, typically with the PORT (e.g., 'https://localhost:3000')
     'https://notebook-i39o.onrender.com',
-    //'https://www.google.com',
-    
-    // Add other host URLs that are allowed to access your server (e.g., 'https://example.com')
-    ''
 ]
 
 module.exports = allowedOrigins
